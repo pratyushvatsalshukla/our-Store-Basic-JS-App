c21e20dbeb37b26f480e8ee4df8ca90b7afd5f26
Pratyush Vatsal Shukla <54927584+pratyushvatsalshukla@users.noreply.github.com>
2021-07-06 11:41:16 +0530

    Re-Uploaded OurStore App

---
+++ Actions.js
@@ -0,0 +1,12 @@
+function SearchRes(){
+
+    var InputRes = document.forms["SearchBarInput"]["SearchValue"].value ;
+    var ResultArea = document.getElementById("ResultArea") ;
+    ResultArea.addEventListener("oninput", SearchResult ) ;
+
+}
+function SearchResult(){
+
+    
+
+}
